8d45f8df58573aef1920e53c773f4c0505c67036
paula borges <paulabzambeli@gmail.com>
2023-12-16 14:16:53 -0300

    categoria

---
+++ src/controladores/categoria.js
@@ -0,0 +1,20 @@
+const pool = require("../conexao");
+
+const listarCategorias = async (req, res) => {
+    
+    try{
+
+    const categorias = await pool.query('select * from categorias')
+    
+        return res.status(200).json(categorias.rows)
+    
+    }catch{
+        return res.status(500).json({mensagem: "Erro interno do servidor" });
+        }
+    }
+
+    
+module.exports = {
+    listarCategorias,
+    
+}
